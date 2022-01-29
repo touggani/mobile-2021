4029221b4c5a92814a09381b52850ec563ee43d5
guidezSeb <sebastien.guidez@lacatholille.fr>
2022-01-29 15:17:34 +0100

    progress page image (single film)

--- mobileapp/lib/screens/image.dart
+++ mobileapp/lib/screens/image.dart
@@ -14,6 +14,12 @@ class MyImage extends StatefulWidget {
   State<MyImage> createState() => _MyImageState();
 }
 
+String durationToString(int minutes) {
+  var d = Duration(minutes:minutes);
+  List<String> parts = d.toString().split(':');
+  return '${parts[0].padLeft(2, '0')}:${parts[1].padLeft(2, '0')}';
+}
+
 class _MyImageState extends State<MyImage> {
   bool _isLikeOn = false;
 
@@ -50,7 +56,7 @@ class _MyImageState extends State<MyImage> {
         return <Widget>[
           SliverAppBar(
             foregroundColor: Colors.black,
-            expandedHeight: MediaQuery.of(context).size.height / 2,
+            expandedHeight: MediaQuery.of(context).size.height / 3,
             backgroundColor: Colors.transparent,
             floating: false,
             pinned: true,
@@ -60,18 +66,14 @@ class _MyImageState extends State<MyImage> {
 
                 //titlePadding: EdgeInsetsDirectional.all(0),
                 title: Stack(children: [
-                  Text(film.title!,
+                  /* Text(film.title!,
                       style: GoogleFonts.roboto(
                         //color: CupertinoColors.black,
                         foreground: Paint()
                           ..style = PaintingStyle.stroke
                           ..strokeWidth = 3
                           ..color = Colors.orange,
-                      )),
-                  Text(film.title!,
-                      style: GoogleFonts.roboto(
-                        color: CupertinoColors.black,
-                      ))
+                      )),*/
                 ]),
                 background: Image.network(
                   film.backdropPath != null
@@ -84,44 +86,87 @@ class _MyImageState extends State<MyImage> {
       },
       body: Column(
         children: [
-          IconButton(
-            icon: Icon(
-              _isLikeOn == true ? Icons.favorite :
-              Icons.favorite_border,
-              color: Colors.orange,
-            ),
-            onPressed: () {
-              setState(() {
-                _isLikeOn = !_isLikeOn;
-              });
-              final snackBar = SnackBar(
-                content: _isLikeOn == true
-                    ? Text('Vous avez ajouté ' +
-                        film.title.toString() +
-                        ' à vos favoris.')
-                    : Text('Vous avez retiré ' +
-                        film.title.toString() +
-                        ' de vos favoris.'),
-                action: SnackBarAction(
-                  label: 'Annuler',
-                  onPressed: () {
-                    setState(() {
-                      _isLikeOn = !_isLikeOn;
-                    });
-                    final snackBar = SnackBar(
+
+          //Expanded(child: ListView(children: _widgetList(film)))
+          Expanded(child: Column(
+
+            children: [
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 52.0, top: 10.0),
+                      child: Text(
+                          film.title!,
+                          textAlign: TextAlign.center,
+
+                          style: GoogleFonts.roboto(
+                            color: CupertinoColors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w900,
+                          )),
+                    ),
+                  ),
+
+                  IconButton(
+                    padding: const EdgeInsets.only(left: 32.0, top: 10.0, right: 20.0),
+                    icon: Icon(
+                      _isLikeOn == true ? Icons.favorite :
+                      Icons.favorite_border,
+                      color: Colors.orange,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isLikeOn = !_isLikeOn;
+                      });
+                      final snackBar = SnackBar(
                         content: _isLikeOn == true
-                            ? Text(film.title.toString() +
-                                ' a été rajouté à vos favoris.')
-                            : Text(film.title.toString() +
-                                ' a été retiré de vos favoris.'));
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  },
-                ),
-              );
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-            },
-          ),
-          Expanded(child: ListView(children: _widgetList(film)))
+                            ? Text('Vous avez ajouté ' +
+                            film.title.toString() +
+                            ' à vos favoris.')
+                            : Text('Vous avez retiré ' +
+                            film.title.toString() +
+                            ' de vos favoris.'),
+                        action: SnackBarAction(
+                          label: 'Annuler',
+                          onPressed: () {
+                            setState(() {
+                              _isLikeOn = !_isLikeOn;
+                            });
+                            final snackBar = SnackBar(
+                                content: _isLikeOn == true
+                                    ? Text(film.title.toString() +
+                                    ' a été rajouté à vos favoris.')
+                                    : Text(film.title.toString() +
+                                    ' a été retiré de vos favoris.'));
+                            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                          },
+                        ),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                  ),
+                ],
+              ),
+
+              Row( mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text("Durée : ",
+                      style: GoogleFonts.roboto(
+                        color: CupertinoColors.black,
+                        //fontSize: 15,
+                      )),
+                  Text(film.runtime.toString() ,
+                      style: GoogleFonts.roboto(
+                        color: CupertinoColors.black,
+
+                      )),
+                ],
+              )
+
+            ],
+          ))
         ],
       ),
     );
